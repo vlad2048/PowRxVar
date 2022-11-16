d6a6f6a9828432821448851ec2b99cacbf3cdaf4
Vlad Niculescu <vlad.nic@gmail.com>
2022-11-16 14:15:20 +0000

    added some utilities, upgraded deps

--- Libs/PowRxVar.Maybe/RxVarMaybeExt.cs
+++ Libs/PowRxVar.Maybe/RxVarMaybeExt.cs
@@ -5,6 +5,9 @@ using PowMaybe;
 // ReSharper disable once CheckNamespace
 namespace PowRxVar;
 
+/// <summary>
+/// Useful extension methods combining Vars and Maybes
+/// </summary>
 public static class RxVarMaybeExt
 {
 	public static IObservable<T> WhenSome<T>(this IRoVar<Maybe<T>> mayVar) =>
@@ -13,13 +16,20 @@ public static class RxVarMaybeExt
 	public static IObservable<Unit> WhenNone<T>(this IRoVar<Maybe<T>> mayVar) =>
 		mayVar.Where(e => e.IsNone()).ToUnit();
 
+	public static IRoVar<bool> WhenVarSome<T>(this IRoVar<Maybe<T>> v) =>
+		v.SelectVar(e => e.IsSome());
+
+	public static IRoVar<bool> WhenVarNone<T>(this IRoVar<Maybe<T>> v) =>
+		v.SelectVar(e => e.IsNone());
+
+
 	public static IObservable<Maybe<U>> SelectMay<T, U>(this IObservable<Maybe<T>> obs, Func<T, U> fun) =>
 		obs.Select(e => e.Select(fun));
 
 	public static IRoVar<Maybe<U>> SelectVarMay<T, U>(this IRoVar<Maybe<T>> v, Func<T, U> fun) =>
 		v.SelectVar(e => e.Select(fun));
 
-	public static (IRoVar<Maybe<U>>, IDisposable) SelectVarMayWithRefresh<T, U>(this IRoVar<Maybe<T>> v, Func<T, U> fun, IObservable<Unit> whenRefresh) =>
+	public static IRoVar<Maybe<U>> SelectVarMayWithRefresh<T, U>(this IRoVar<Maybe<T>> v, Func<T, U> fun, IObservable<Unit> whenRefresh) =>
 		Var.Make(
 			May.None<U>(),
 			Observable.Merge(
@@ -35,7 +45,7 @@ public static class RxVarMaybeExt
 				)
 				.Delay(TimeSpan.Zero)
 				.SelectMay(fun)
-		);
+		).D(v);
 
 	public static IRoVar<T> FailWith<T>(this IRoVar<Maybe<T>> v, T def) =>
 		v.SelectVar(e => e.FailWith(def));
@@ -53,7 +63,7 @@ public static class RxVarMaybeExt
 			});
 
 	private static IObservable<(Maybe<T>, T)> CombineWithPrevious<T>(this IObservable<T> obs) =>
-		obs.Scan<T, (Maybe<T>, Maybe<T>)>(
+		obs.Scan(
 				(May.None<T>(), May.None<T>()),
 				(t, v) => (t.Item2, May.Some(v))
 			)

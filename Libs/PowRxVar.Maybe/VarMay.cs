46c354719e2ab781f1c707b3ed83d035b6147265
Vlad Niculescu <vlad.nic@gmail.com>
2023-06-13 15:45:11 +0100

    fix

--- Libs/PowRxVar.Maybe/VarMay.cs
+++ Libs/PowRxVar.Maybe/VarMay.cs
@@ -10,10 +10,10 @@ public static class VarMay
 {
 	public static IRwMayVar<T> Make<T>() => new RwMayVar<T>(May.None<T>(), false, "VarMay.Make()");
 	public static IFullRwMayBndVar<T> MakeBnd<T>() => new FullRwMayBndVar<T>(May.None<T>(), false, "VarMay.MakeBnd()");
-	public static (IRoMayVar<T>, IDisposable) Make<T>(IObservable<T> obs)
+	public static (IRoMayVar<T>, IDisposable) Make<T>(IObservable<Maybe<T>> obs)
 	{
 		var v = Make<T>();
-		obs.Select(May.Some).Subscribe(v).D(v);
+		obs.Subscribe(v).D(v);
 		return (v.ToReadOnlyMay(), v);
 	}
 

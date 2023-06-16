0e26f2c6239dea31d2bf89ea8bfad24b4c40920e
Vlad Niculescu <vlad.nic@gmail.com>
2023-06-16 20:17:32 +0100

    added nocheck fun

--- Libs/PowRxVar.Maybe/VarMayNoCheck.cs
+++ Libs/PowRxVar.Maybe/VarMayNoCheck.cs
@@ -8,4 +8,10 @@ public static class VarMayNoCheck
 {
 	public static IRwMayVar<T> Make<T>() => new RwMayVar<T>(May.None<T>(), true, "VarMay.Make()");
 	public static IFullRwMayBndVar<T> MakeBnd<T>() => new FullRwMayBndVar<T>(May.None<T>(), true, "VarMay.MakeBnd()");
+	public static (IRoMayVar<T>, IDisposable) Make<T>(IObservable<Maybe<T>> obs)
+	{
+		var v = Make<T>();
+		obs.Subscribe(v).D(v);
+		return (v.ToReadOnlyMay(), v);
+	}
 }
